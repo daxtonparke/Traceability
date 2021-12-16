c70661bbee07d6fd6a937531ab1aee89d5cd4ec4
daxtonparke <daxtonparke@gmail.com>
2021-12-16 14:26:28 -0700

    rollbar

--- index.js
+++ index.js
@@ -4,9 +4,18 @@ const path = require('path')
 const app = express()
 app.use(express.json())
 
+var Rollbar = require("rollbar");
+var rollbar = new Rollbar({
+  accessToken: '3079bdf26d644a3faea00027df685975',
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
+
 app.get('/', (req,res)=> {
     res.sendFile(path.join(__dirname, '/public/index.html'))
+    rollbar.info('html file served successfully.')
 })
+
 
 const port = process.env.PORT || 4004
 
